db7c1fa65f8bf9fa68d443f01cc75180f4a63b4d
Joshs-Projects <74363804+Joshs-Projects@users.noreply.github.com>
2021-09-22 19:08:37 +0100

    Added a few comments on what the code is doing.

--- TicTacToe/Source.cpp
+++ TicTacToe/Source.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 int displayBoard(vector<vector<char>> board) {
+	//cout upside down to effectivly invert the y coords
+	//This makes y=1 the bottom and y = 3 the top
 	for (int i = 2; i >= 0; i--) {
 		cout << board[i][0] << '|' << board[i][1] << '|' << board[i][2] << endl;
 		if (i == 0) {
@@ -19,17 +21,20 @@ int displayBoard(vector<vector<char>> board) {
 
 //check for a winner
 bool isWin(vector<vector<char>> board) {
-
 	for (int i = 0; i < 3; i++) {
+		//Checks all columns
 		if ((board[i][0] == board[i][1]) && (board[i][1] == board[i][2]) && (board[i][0] != ' ')) {
 			return true;
 		}
+		//Checks all rows
 		else if ((board[0][i] == board[1][i]) && (board[1][i] == board[2][i]) && (board[0][i] != ' ')) {
 			return true;
 		}
+		//Checks diagonal bottom-left to top-right
 		else if ((board[0][0] == board[1][1]) && (board[1][1] == board[2][2]) && (board[1][1] != ' ')) {
 			return true;
 		}
+		//Checks diagonal top-left to bottom-right
 		else if ((board[0][2] == board[1][1]) && (board[1][1] == board[2][0]) && (board[1][1] != ' ')) {
 			return true;
 		}	
@@ -38,9 +43,11 @@ bool isWin(vector<vector<char>> board) {
 }
 
 bool isMove(vector<vector<char>> board, int x_coord, int y_coord) {
+	//Checks that x and y are inside the grid
 	if ((x_coord < 1 || x_coord > 3) || (y_coord < 1 || y_coord > 3)) {
 		return false;
 	}
+	//Checks that x and y isn't currently filled
 	if ((board[y_coord - 1][x_coord - 1] == 'x') || (board[y_coord - 1][x_coord - 1] == 'o')) {
 		return false;
 	}
@@ -59,13 +66,9 @@ int main() {
 	char playerTurn = 'x';
 	vector<vector<char>> board = { {' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '} };
 
-	isWin(board);
-
-	displayBoard(board);
-
 	int turnNumber = 0;
 
-	//while no winner and a space to go in
+	//While no winner and a space to go in keep playing
 	while (turnNumber < 9 && isWin(board) == false) {
 		displayBoard(board);
 
@@ -76,21 +79,17 @@ int main() {
 		cout << "Please give the Y-Coordinate of where you want to go" << endl;
 		cin >> y_coord;
 
-		//Do this to invert the y_coord so y_coord of 1 is at the bottom and y_coord of 3 is at the top
-		//y_coord = 3 - y_coord;
-
 		while (!isMove(board, x_coord, y_coord)) {
 			cout << "This is not a valid move please try again" << endl;
 			cout << "Please give the X-Coordinate of where you want to go" << endl;
 			cin >> x_coord;
 			cout << "Please give the Y-Coordinate of where you want to go" << endl;
 			cin >> y_coord;
-
-			//Do this to invert the y_coord so y_coord of 1 is at the bottom and y_coord of 3 is at the top
-			//y_coord = 3 - y_coord;
 		}
+
 		board = makeMove(board, x_coord, y_coord, playerTurn);
 
+		//Adds to the total turns as if the turns = 9 all boxes are filled and game is over
 		turnNumber = turnNumber + 1;
 		if (playerTurn == 'x') {
 			playerTurn = 'o';
